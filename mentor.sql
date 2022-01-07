9de0d6e2681c5f27581f34586d8ffc4ad827f7cc
barath sam <barathsam555.p@gmail.com>
2022-01-07 16:34:13 +0530

    first commit

---
+++ mentor.sql
@@ -0,0 +1,6 @@
+create table Zenclass.mentor (mentorId int primary key auto_increment, mentorName varchar(100) not null);
+INSERT INTO Zenclass.mentor ( mentorName)
+VALUES ('guru'),
+('teacher'),
+('vathiyar');
+select * from zenclass.mentor;
